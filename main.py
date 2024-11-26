08fe505f9ff30088fc55e645667aa4480511b2c5
vitoria174 <vitoriaalbuquerque174@gmail.com>
2024-11-26 20:04:04 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#gestao de tarefas
+
+
+while True:
+    print('1-Criar Tarefa\n'
+          '2-Visualizar\n'
+          '3-Atualizar\n'
+          '4-Deletar [Completas]\n'
+          '0-Sair')
+    escolha=int(input('Digite uma opção:'))
+
+    if escolha ==0:
+        break
